f24f1c9de358327390ac79aafbf5563071d042e1
metalmount <sokunbitaiwo82@gmail.com>
2017-04-08 09:54:17 +0100

    correct my fizzBuzz function

--- app/fizzBuzz.js
+++ app/fizzBuzz.js
@@ -1,29 +1,36 @@
-function fizzBuzz(num){
+'use strict'
+
+module.exports={
+    fizzBuzz: function(num){
 
    
-    var N =num;
+        var N =num;
 
-    if((N >=1 && N<10000 ) && typeof(N)==='number'){  
-        if(N%3===0 && N%5 ===0 ){
-            return("FizzBuzz");
-        }
-        else if(N%3===0){
-           return("Fizz");
-        }
-        else if(N%5 ===0 ){
-            return("Buzz");
-        }
+        if((N >=1 && N<10000 ) && typeof(N)==='number'){  
+            if(N%3===0 && N%5 ===0 ){
+                return("FizzBuzz");
+            }
+            else if(N%3===0){
+               return("Fizz");
+            }
+            else if(N%5 ===0 ){
+                return("Buzz");
+            }
 
+            else{
+                return(N)
+            }
+      
+                
+            
+        }
+                
         else{
-            return(N)
+            return(undefined);
+          }
         }
-  
-            
-        
-    }
-            
-    else{
-        return(undefined);
-      }
-    }
-    
+
+}
+
+
+
