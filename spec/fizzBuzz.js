a7ce4dc2e74285e372885c024918a0fdc47e1275
metalmount <sokunbitaiwo82@gmail.com>
2017-04-08 13:29:38 +0100

    third commit foe the fizzBuzz app

---
+++ spec/fizzBuzz.js
@@ -0,0 +1,36 @@
+'use strict'
+var exports = module.exports={};
+
+exports.fizzBuzz=function(num){
+
+   
+    var N =num;
+
+    if((N >=1 && N<10000 ) && typeof(N)==='number'){  
+        if(N%3===0 && N%5 ===0 ){
+            return("FizzBuzz");
+        }
+        else if(N%3===0){
+            return("Fizz");
+        }
+        else if(N%5 ===0 ){
+            return("Buzz");
+        }
+
+        else{
+            return(N)
+        }
+      
+                
+            
+    }
+                
+    else{
+        return(undefined);
+        }
+};
+
+
+
+
+
