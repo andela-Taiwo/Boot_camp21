118f5379e0e4658f112ee9637d74b2d664e5e904
metalmount <sokunbitaiwo82@gmail.com>
2017-04-08 22:41:01 +0100

    Adding update ariGeo app

--- app/aritGeo.js
+++ app/aritGeo.js
@@ -1,31 +1,45 @@
-function aritGeo(num){
+function gcd(m,n){
+    while(m%n != 0){
+       var oldm = m ; 
+       var oldn = n ;
+
+       var m = oldn ; 
+       var n = oldm%oldn ; 
+    }
+    return(n);
+
+
+}
+
+var exports =module .exports = {};
+
+exports.aritGeo=function(num){
 	
 	if(num.length === 0){
 		return(0);
 	}
 	else{
 		for(var i=0 ; i<num.length; i++){
-		  
-		  for(var j=i+1; j<num.length; j++){
-		    if(num[j] -num[i] === num[j+1] - num[j]){
-		      return("Arithmetic");
-					}
-				else if(num[j]/num[i] === num[j+i]/num[j]){
-				  return("Geometric");
+			for(var j=i+1; j<num.length; j++){
+				if(num[j]-num[i] === num[j+1] - num[j]){
+					return("Arithmetic");
 				}
-			  else{
-			    return(-1);
+				else if(gcd(num[j],num[i])=== gcd(num[j+i],num[j])){
+					return("Geometric");
+				}
+				else{
+					return(-1);
 			    
-			  }
+			  	}
 
 
-				}
 			}
+		}
 
 	}
 
 
-}
+};
 
 
 
