06483969a34ada41847eee4fe220c8ef15f2f8d1
metalmount <sokunbitaiwo82@gmail.com>
2017-04-08 15:47:52 +0100

    Update ariGeo app

--- app/aritGeo.js
+++ app/aritGeo.js
@@ -1,35 +1,6 @@
 function aritGeo(num){
 	
 	if(num.length === 0){
-		return(0)
-	}
-
-	else{
-		for(var i=0 ; i<num.length; i++){
-			if(typeof(num[i]) !== 'number'){
-				return(-1);
-				
-			}
-			else{
-				for(var j=i+1; j<num.length; j++){
-					if(num[j] -num[i] !== num[j+1] - num[j]){
-						return("Geometric");
-					}
-				return("Arithmetic");
-
-
-				}
-			}
-
-
-		}
-	}
-}
-
-
-function aritGeo(num){
-	
-	if(num.length === 0){
 		return(0);
 	}
 	else{
